a84c595ac9ee5c75760bf7ace596f3980dc94b4c
Alexander Shvets <all.shvets.st@gmail.com>
2021-11-01 17:24:47 +0300

    fix and update Calculator

--- calc/main.js
+++ calc/main.js
@@ -1,12 +1,8 @@
-const sum  = (a, b) => a + b;
-const sub  = (a, b) => a - b;
-const mult = (a, b) => a * b;
-const div  = (a, b) => a / b;
 
 const getErrorMessage = (message) => `Error text: ${message}`;
 
 function checkData(a, b, operator) {
-  const isNotValidNumbers = ( typeof a !== 'number' || typeof b !== 'number' );
+  const isNotValidNumbers = ( typeof a !== 'number' || typeof b !== 'number' || isNaN(a) || isNaN(b) );
   const isNotValidOperator = !operator;
   
   return isNotValidNumbers || isNotValidOperator;
@@ -21,13 +17,13 @@ function calc(a, b, operator) {
   
   switch (operator) {
     case 'sum':
-      return sum(a, b);
+      return a + b;
     case 'sub':
-      return sub(a, b);
+      return a - b;
     case 'mult':
-      return mult(a, b);
+      return a * b;
     case 'div':
-      return div(a, b);
+      return a / b;
     default:
       return getErrorMessage('unknown operation');
   }
@@ -46,14 +42,18 @@ console.log( calc(3, '', 'sum') );
 console.log( calc('some string', 3, 'sum') );
 console.log( calc(3, 'some string', 'sub') );
 console.log( calc('0', '3', 'sum') );
-console.log( calc( 1, '3', 'sum') );
-console.log( calc( '3', 1, 'sum') );
-console.log( calc( 3, undefined, 'sum') );
-console.log( calc( undefined, 3, 'sum') );
-console.log( calc( 3, null, 'sum') );
-console.log( calc( null, 3, 'sum') );
+console.log( calc(1, '3', 'sum') );
+console.log( calc('3', 1, 'sum') );
+console.log( calc(3, undefined, 'sum') );
+console.log( calc(undefined, 3, 'sum') );
+console.log( calc(3, null, 'sum') );
+console.log( calc(null, 3, 'sum') );
 
+console.log( calc(0, 2, 'sub') );
 console.log( calc(3, 2, 'sum') );
 console.log( calc(2, 3, 'sub') );
 console.log( calc(3, 4, 'mult') );
 console.log( calc(6, 0, 'div') );
+
+console.log( calc(NaN, 0, 'div') );
+console.log( calc(1, NaN, 'sum') );
