73906527f8a25fc6ca2b02bdb4c703be5a5a6a42
Alexander Shvets <all.shvets.st@gmail.com>
2021-11-01 15:42:29 +0300

    fix

--- calc/main.js
+++ calc/main.js
@@ -3,7 +3,7 @@ const sub  = (a, b) => a - b;
 const mult = (a, b) => a * b;
 const div  = (a, b) => a / b;
 
-const getErrorMessage = (message) => `Text error: ${message}`;
+const getErrorMessage = (message) => `Error text: ${message}`;
 
 function checkData(a, b, operator) {
   const isNotValidOperator = !operator;
@@ -27,36 +27,10 @@ function calc(a, b, operator) {
     case 'mult':
       return mult(a, b);
     case 'div':
-      if (b === 0) {
-        return getErrorMessage('you cannot divide by 0!');
-      }
       return div(a, b);
     default:
       return getErrorMessage('unknown operation');
   }
 }
 
-
-console.log( calc(2, 3, 'sum ') );
-console.log( calc(2, 3, ' sum ') );
-console.log( calc(2, 3, 'some operator') );
-console.log( calc(2, 3) );
-console.log( calc(2, 3, '') );
-
-console.log( calc('', 3, 'sum') );
-console.log( calc(3, '', 'sum') );
-
-console.log( calc('some string', 3, 'sum') );
-console.log( calc(3, 'some string', 'sub') );
-console.log( calc('0', '3', 'sum') );
-console.log( calc( 1, '3', 'sum') );
-console.log( calc( '3', 1, 'sum') );
-console.log( calc( 3, undefined, 'sum') );
-console.log( calc( undefined, 3, 'sum') );
-console.log( calc( 3, null, 'sum') );
-console.log( calc( null, 3, 'sum') );
-
-console.log( calc(3, 2, 'sum') );
-console.log( calc(2, 3, 'sub') );
-console.log( calc(3, 4, 'mult') );
-console.log( calc(6, 0, 'div') );
+console.log( calc(2, 3, 'sum') );
