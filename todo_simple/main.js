fa05dafb4174bafe5709168e34a09c35c2fe5c75
Alexander Shvets <all.shvets.st@gmail.com>
2021-11-06 19:42:22 +0300

    Update

--- todo_simple/main.js
+++ todo_simple/main.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const list = {
-  "create a task": "In Progress",
-  "make a bed": "Done",
-  "write a post": "To Do",
+  // "create a task": "In Progress",
+  // "create a tasks": "In Progress",
+  // "make a bed": "Done",
+  // "make a beds": "Done",
+  // "write a post": "To Do",
+  // "write a posts": "To Do",
 };
 
 function changeStatus(task, status) {
@@ -19,47 +22,40 @@ function deleteTask(task) {
 }
 
 function showList(list) {
-  const resultObj = {};
-  let strTodo = '';
-  let strProgress = '';
-  let strDone = '';
+  const resultObj = {
+    'To Do': '',
+    'In Progress': '',
+    'Done': '',
+  };
 
   for (let task in list) {
     const status = list[task];
 
     switch (status) {
       case 'To Do':
-        strTodo += ' "' + task + '",\n';
+        resultObj[status] += ' "' + task + '",\n';
         break;
       case 'In Progress':
-        strProgress += ' "' + task + '",\n';
+        resultObj[status] += ' "' + task + '",\n';
         break;
       case 'Done':
-        strDone += ' "' + task + '",\n';
+        resultObj[status] += ' "' + task + '",\n';
         break;
-    }
-  }
-
-  resultObj['To Do'] = strTodo;
-  resultObj['In Progress'] = strProgress;
-  resultObj['Done'] = strDone;
-
-  for (let key in resultObj) {
-    if (!resultObj[key]) {
-      resultObj[key] = ' -\n';
     }
   }
 
   let result = '';
   
   for (let key in resultObj) {
-    result += key + ':\n' + resultObj[key];
+    result += (resultObj[key]) ?
+      key + ':\n' + resultObj[key] :
+      key + ':\n -\n';
   }
 
   console.log( result.trim() );
 }
 
-changeStatus('create a task', 'Done');
+// changeStatus('create a task', 'Done');
 addTask('have a walk');
-deleteTask('have a walk');
+// deleteTask('have a walk');
 showList(list);
