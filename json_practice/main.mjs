0652cb431d3ec0d53db991b922262d9fa1e58a75
Alexander Shvets <all.shvets.st@gmail.com>
2021-12-08 20:53:35 +0300

    Add JSON-practice

---
+++ json_practice/main.mjs
@@ -0,0 +1,22 @@
+import data from './data.json';
+
+
+function showUsersInfo() {
+  data.users.forEach(function (user) {
+    let userInfo = `${user.firstName}, born ${user.dateOfBirth} - ${user.knowsAs}`;
+
+    console.log(userInfo);
+  });
+}
+
+showUsersInfo();
+
+
+function getListUsersName() {
+  const json = JSON.stringify(data, ['users', 'firstName', 'lastName']);
+  const users = JSON.parse(json);
+
+  return users.users;
+}
+
+console.log( getListUsersName() );
