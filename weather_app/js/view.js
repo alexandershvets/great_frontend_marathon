06e8de42989aa0c63ba8e33468f3adc050729c53
Alexander Shvets <all.shvets.st@gmail.com>
2021-12-18 18:07:36 +0300

    Update

--- weather_app/js/view.js
+++ weather_app/js/view.js
@@ -1,15 +1,10 @@
 export const UI_ELEMENTS = {
   FORMS: document.querySelectorAll('.form'),
-
   SERACH_INPUT: document.querySelector('.search-form-weather__input'),
   TEMP: document.querySelector('.now-item__temperature'),
   SITY_NAME: document.querySelector('.now-item__town'),
   WEATHER_ICON: document.querySelector('.now-item__icon img'),
-
   CITIES_LIST: document.querySelector('.locations-form-weather__list'),
   ADD_SITY_BTN: document.querySelector('.now-item__like'),
-
-  // TAB_BUTTONS: document.querySelectorAll('.info-weather__tab'),
-  // CONTENT_ITEMS: document.querySelectorAll('.info-weather__item'),
   TAB: document.querySelector('.tab')
 };
