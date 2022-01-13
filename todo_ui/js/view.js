421b63886c23dee1ae25912c9f5a8a969d673f72
Alexander Shvets <all.shvets.st@gmail.com>
2022-01-13 12:41:09 +0300

    Add view.js

---
+++ todo_ui/js/view.js
@@ -0,0 +1,6 @@
+export const UI_ELEMENTS = {
+  forms: document.querySelectorAll('.todo__field'),
+  // inputs: document.querySelectorAll('.todo__input'),
+  addTaskBtn: document.querySelectorAll('.task__cross_add'),
+  // tasksBody: document.querySelectorAll('.todo__body')
+};
