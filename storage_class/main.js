b159061bd6193925cfd5464a99a59633da1d1249
Alexander Shvets <all.shvets.st@gmail.com>
2022-02-06 02:47:25 +0300

    Update

--- storage_class/main.js
+++ storage_class/main.js
@@ -3,7 +3,7 @@ class Storage {
     this.key = key;
     this.defaultValue = defaultValue;
     this.storageType = storageType;
-    this.set(this.defaultValue);
+    this.set(defaultValue);
   }
 
   get() {
@@ -47,23 +47,23 @@ const currentCity = new Storage('currentCity', { defaultValue: 'Murmansk' });
 
 /* Tests */
 
-// console.log(names);
-// console.log(favorites);
-// console.log(currentCity);
+console.log(names);
+console.log(favorites);
+console.log(currentCity);
 
-// // names.set('value1');
-// names.set({cityName: 'Moscow'});
-// // names.set([1, false, 'some']);
-// // names.set();
-// console.log( names.get() );
-// // names.clear();
-// console.log( names.isEmpty() );
+// names.set('value1');
+names.set({cityName: 'Moscow'});
+// names.set([1, false, 'some']);
+// names.set();
+console.log( names.get() );
+// names.clear();
+console.log( names.isEmpty() );
 
-// favorites.set('value2');
-// // storage2.set(null);
-// console.log( favorites.get() );
-// // storage2.clear();
-// console.log( favorites.isEmpty() );
+favorites.set('value2');
+// storage2.set(null);
+console.log( favorites.get() );
+// storage2.clear();
+console.log( favorites.isEmpty() );
 
-// console.log(localStorage);
-// console.log(sessionStorage);
+console.log(localStorage);
+console.log(sessionStorage);
