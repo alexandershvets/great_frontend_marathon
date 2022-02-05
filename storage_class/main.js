f1cd76e212185e3ed40a376af4377176be9cdee3
Alexander Shvets <all.shvets.st@gmail.com>
2022-02-06 02:47:48 +0300

    Update

--- storage_class/main.js
+++ storage_class/main.js
@@ -47,23 +47,23 @@ const currentCity = new Storage('currentCity', { defaultValue: 'Murmansk' });
 
 /* Tests */
 
-console.log(names);
-console.log(favorites);
-console.log(currentCity);
+// console.log(names);
+// console.log(favorites);
+// console.log(currentCity);
 
-// names.set('value1');
-names.set({cityName: 'Moscow'});
-// names.set([1, false, 'some']);
-// names.set();
-console.log( names.get() );
-// names.clear();
-console.log( names.isEmpty() );
+// // names.set('value1');
+// names.set({cityName: 'Moscow'});
+// // names.set([1, false, 'some']);
+// // names.set();
+// console.log( names.get() );
+// // names.clear();
+// console.log( names.isEmpty() );
 
-favorites.set('value2');
-// storage2.set(null);
-console.log( favorites.get() );
-// storage2.clear();
-console.log( favorites.isEmpty() );
+// favorites.set('value2');
+// // storage2.set(null);
+// console.log( favorites.get() );
+// // storage2.clear();
+// console.log( favorites.isEmpty() );
 
-console.log(localStorage);
-console.log(sessionStorage);
+// console.log(localStorage);
+// console.log(sessionStorage);
